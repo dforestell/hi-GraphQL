1a4676906d18cd4753f94ba2c6ee3103548bb8cf
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-02-07 16:19:59 -0600

    add a db.json file to simulate an outside api, add a couple of users to it, and add a script to the package.json telling it what to watch when we run json server via the terminal

--- schema/schema.js
+++ schema/schema.js
@@ -7,10 +7,6 @@
  	GraphQLSchema
  } = graphql;
 
- const users = [
- 	{ id: '23', firstName: 'Bill', age: 20 },
- 	{ id: '47', firstName: 'Samantha', age: 21 }
- ]
 
  const UserType = new GraphQLObjectType({
  	name: 'User',
